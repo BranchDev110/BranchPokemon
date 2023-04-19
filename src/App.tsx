0ea83ce19ecd0735e1866a3ecb4dc434cda722a6
BranchDev110 <steven.fredericks.dev110@gmail.com>
2023-04-19 12:27:05 -0400

    Fetch Data from Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemons, setPokemons] = useState<any>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const { results } = await response.json();
+      const detailResponse = await Promise.all(results.map(async ({url}: {url: string}) => await fetch(url)));
+      const pokemon = await Promise.all(detailResponse.map(item => item.json()));
+      setPokemons(pokemon);
+    }
+    fetchData();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        pokemons.map((pokemon: any) => <div>{pokemon.name}</div>)
+      }
     </div>
   );
 }
