4ae673fcbeb8a486c3aa5f8ee1c7aebcd822c5c4
BranchDev110 <steven.fredericks.dev110@gmail.com>
2023-04-19 20:17:16 -0400

    Implement Search Feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from './app/store';
-import { setPokemon } from './app/slice/pokemonReducer';
+import { setPokemon, setSearchedResult } from './app/slice/pokemonReducer';
+import { setHistory } from './app/slice/historyReducer';
 
 function App() {
-  const pokemons = useSelector((state: RootState) => state.pokemon);
+  const [searchText, setSearchText] = useState<string>("");
+  const {pokemons, searchedResult} = useSelector((state: RootState) => state.pokemon);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,10 +20,24 @@ function App() {
     }
     fetchData();
   }, [])
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const newSearchedResult = pokemons.filter((pokemon: any) => pokemon.name.toLowerCase().includes(e.currentTarget.value.toLowerCase()));
+    setSearchText(e.currentTarget.value);
+    dispatch(setSearchedResult(newSearchedResult));
+  }
+
+  const handleClick = () => {
+    const newSearchedResult = pokemons.filter((pokemon: any) => pokemon.name.toLowerCase().includes(searchText.toLowerCase()));
+    dispatch(setSearchedResult(newSearchedResult));
+    dispatch(setHistory({key: searchText, value: newSearchedResult, date: new Date().toISOString()}));
+  }
   return (
     <div className="App">
+      <input onChange={handleChange} value={searchText}/>
+      <button onClick={handleClick}>Search</button>
       {
-        pokemons.map((pokemon: any) => <div>{pokemon.name}</div>)
+        searchedResult.map((pokemon: any) => <div>{pokemon.name}</div>)
       }
     </div>
   );
